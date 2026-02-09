5b45409a4408f035b019fb2e002969cd828715e4
Subhajit Bag <u2subhajit@gmail.com>
2026-02-09 19:49:01 +0530

    add output.png'

--- Day1 - Product & Cart Management Module/modules/es6.js
+++ Day1 - Product & Cart Management Module/modules/es6.js
@@ -5,12 +5,6 @@ const generateId = () => {
     return Math.floor(Math.random() * 10000);
 };
 
-// Format product output
-const formatProduct = ({ id, name, price, stock }) => {
-    return `ID: ${id} | Name: ${name} | Price: ₹${price} | Stock: ${stock}`;
-};
-
 module.exports = {
-    generateId,
-    formatProduct
+    generateId
 };
