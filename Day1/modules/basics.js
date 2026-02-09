158d75605f0ce2610b362f0985cbf51574b296dc
Subhajit Bag <u2subhajit@gmail.com>
2026-02-09 16:53:57 +0530

    Add basics module for price and discount logic

--- Day1/modules/basics.js
+++ Day1/modules/basics.js
@@ -1,1 +1,14 @@
+//Price Calculation
+function calculatePrice(price, quantity){
+    return price * quantity;
+}
 
+//Discount Feature
+function applyDiscount(total, Discount){
+    return total - (total * discount / 100);
+}
+
+module.exports = {
+    calculatePrice,
+    applyDiscount
+};
